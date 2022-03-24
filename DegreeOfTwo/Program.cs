ad05ac919fca02d4b6eb1002e6c9fd634712f68e
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-24 23:15:55 +0300

    Добавьте файлы проекта.

---
+++ DegreeOfTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        int minimumRandomNumbers = 10;
+        int maximumRandomNumbers = 30;
+        int numberRaisedToPower = 2;
+        Random random = new Random();
+        int randomNumbers = random.Next(minimumRandomNumbers, maximumRandomNumbers);
+        int power = 1;
+        int resultOfExponentiation = 2;
+
+        for (int i = power; power < randomNumbers; power++)
+        {
+            if (resultOfExponentiation <= randomNumbers)
+            {
+                resultOfExponentiation = numberRaisedToPower * resultOfExponentiation;
+            }
+            else
+            {
+                break;
+            }
+        }
+        Console.WriteLine($"Минимальная степень двойки, превосходящая число {randomNumbers} будет {numberRaisedToPower}^{power}. {randomNumbers}<{resultOfExponentiation}");
+    }
+}
